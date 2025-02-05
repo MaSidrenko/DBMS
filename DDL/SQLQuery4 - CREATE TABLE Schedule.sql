9146f6c4c6f76bc1028fba5537d362961a2416ab
Maxim Sidorenko <Maxim Sidorenko@DESKTOP-IIFDPU3>
2025-02-05 18:58:19 +0300

    CREATE DONE

--- DDL/SQLQuery4 - CREATE TABLE Schedule.sql
+++ DDL/SQLQuery4 - CREATE TABLE Schedule.sql
@@ -1,15 +1,39 @@
 USE PV_319_DDL;
 GO
 
-CREATE TABLE Schedule
+--CREATE TABLE Schedule
+--(
+--	lesson_id		BIGINT			PRIMARY KEY,
+--	[date]			DATE			NOT NULL,
+--	[time]			TIME			NOT NULL,
+--	[group]			INT				NOT NULL,
+--	discipline		SMALLINT		NOT NULL,
+--	teacher			INT				NOT NULL,
+--	[subject]		NVARCHAR(256)	NULL,
+--	spent			BIT				NOT NULL,
+--	CONSTRAINT FK_Schedule_Disciplines		FOREIGN KEY (discipline)		REFERENCES Disciplines(discipline_id),
+--	CONSTRAINT FK_Schedule_Groups			FOREIGN KEY ([group])			REFERENCES Groups(group_id),
+--	CONSTRAINT FK_Schedule_Teachers			FOREIGN KEY (teacher)			REFERENCES Teachers(teacher_id)
+--);
+
+--CREATE TABLE Grades
+--(
+--	student		INT,
+--	lesson		BIGINT,
+--	present		BIT			NULL,
+--	grade_1		TINYINT		NULL,
+--	grade_2		TINYINT		NULL,
+--	PRIMARY KEY (student, lesson),
+--	CONSTRAINT FK_Grades_Students			FOREIGN KEY (student)		REFERENCES Students(student_id),
+--	CONSTRAINT FK_Grades_Schedule			FOREIGN KEY (lesson)		REFERENCES Schedule(lesson_id)
+--);
+
+CREATE TABLE Exams
 (
-	lesson_id		BIGINT			PRIMARY KEY,
-	[date]			DATE			NOT NULL,
-	[time]			TIME			NOT NULL,
-	[group]			INT				NOT NULL,
-	discipline		SMALLINT		NOT NULL,
-	teacher			INT				NOT NULL,
-	[subject]		NVARCHAR(256)	NULL,
-	spent			BIT				NOT NULL,
-	CONSTRAINT FK_
+	student			INT,
+	lesson			BIGINT,
+	grade			TINYINT		NULL,
+	PRIMARY KEY(student, lesson),
+	CONSTRAINT FK_Exams_Students		FOREIGN KEY (student)			REFERENCES	Students(student_id),
+	CONSTRAINT FK_Exams_Schedule		FOREIGN KEY (lesson)			REFERENCES	Schedule(lesson_id)
 );
